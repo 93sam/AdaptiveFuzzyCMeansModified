2372ceccb6d465b8ae7dd139333b5de324fdecc1
sahil <sahilgovel@gmail.com>
2014-08-10 17:28:50 +0530

    C matrix initialized

--- fcm.py
+++ fcm.py
@@ -5,17 +5,25 @@ from reader import Reader
 class FCM():
 	def __init__(self,n_clusters,epsilon=0.05,max_iter=-1):
 		self.m = 2
+
 		read = Reader('irisdata2.txt')
 		X,y = read.get_data()
 		self.data_shape = X.shape
 		rows , cols = self.data_shape
+		
 		self.U = []
 		for i in range(rows):
 			index = i % n_clusters
 			l = [ 0 for j in range(n_clusters) ]
 			l[index] = 1
 			self.U.append(l)
-		self.U = np.array(self.U).astype(np.float)	
+		self.U = np.array(self.U).astype(np.float)
+		
+		self.C = []
+		l = [ 0 for j in range(cols) ]
+		for i in range(n_clusters):
+			self.C.append(l)
+		self.C = np.array(self.C).astype(np.float)
 
 a = FCM(4)
 
